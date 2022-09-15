69beaab52d6007efb3d6adf7961742dc19470e57
Leonardo Velazquez <leo.vel330@gmail.com>
2022-09-15 17:38:20 -0500

    Creación del manejador

---
+++ AccedoDatos/AccesoTienda.cs
@@ -0,0 +1,29 @@
+﻿using ConectarRedReal;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccedoDatos
+{
+    public class AccesoTienda : IEntidades
+    {
+        Base b = new Base("localhost","root","","producto");
+        public void Borrar(dynamic Entidad)
+        {
+            
+        }
+
+        public void Guardar(dynamic Entidad)
+        {
+            
+        }
+
+        public DataSet Mostrar(string filtro)
+        {
+            return b.Obtener(string.Format("call showproductos('%{0}%')", filtro), "producto");
+        }
+    }
+}
---
+++ Manejador/ManejadorTienda.cs
@@ -0,0 +1,37 @@
+﻿using AccedoDatos;
+using Crud;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manejador
+{
+    public class ManejadorTienda : IManejador
+    {
+        AccesoTienda t = new AccesoTienda();
+        Graficos g = new Graficos();
+        public void Borrar(dynamic Entidad)
+        {
+            
+        }
+
+        public void Guardar(dynamic Entidad)
+        {
+            
+        }
+
+        public void Mostrar(DataGridView tabla, string filtro)
+        {
+            tabla.Columns.Clear();
+            tabla.RowTemplate.Height = 30;
+            tabla.DataSource = t.Mostrar(filtro).Tables["pais"];
+            tabla.Columns.Insert(3, g.Boton("Editar", Color.Green));
+            tabla.Columns.Insert(4, g.Boton("Borrar", Color.Red));
+            tabla.Columns[0].Visible = false;
+        }
+    }
+}
